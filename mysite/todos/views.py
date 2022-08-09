45901d2aa08dbe000b520908e5e002f0341670af
chikuwa1 <b027ff@yamaguchi-u.ac.jp>
2022-08-09 19:03:50 +0900

    css added for index.html

--- mysite/todos/views.py
+++ mysite/todos/views.py
@@ -3,22 +3,22 @@ from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
-# from .forms import CreateForm
+
 
 from .models import Todo
 
 # 締め切り順にTodoリストを表示する機能
 # def index(request):
-#     latest_todo_list = Todo.objects.order_by('deadline')[:5]
+#     nearest_todo_list = Todo.objects.order_by('deadline')[:5]
 #     template = loader.get_template('todos/index.html')
 #     context = {
-#         'latest_todo_list': latest_todo_list
+#         'nearest_todo_list': nearest_todo_list
 #     }
 #     return HttpResponse(template.render(context, request))
 
 class IndexView(generic.ListView):
     template_name = 'todos/index.html'
-    context_object_name = 'latest_todo_list'
+    context_object_name = 'nearest_todo_list'
     
     def get_queryset(self):
         return Todo.objects.order_by('deadline')
@@ -41,23 +41,23 @@ def create(request):
     return render(request, 'todos/create.html')
 
 
-# Todoを終了した際の表示
+# Todoを終了した際の表示 #これいらない、削除
 def complete(request,todo_id):
     return HttpResponse('completed!')
 # def CompleteView(request, todo_id):
 #     response = "Congratulation! You did todo %s!"
 #     return HttpResponse(response % todo_id)
 
-def end(request):
+def createConf(request):
     # todo = get_object_or_404(Todo)
     Todo.objects.create(name=request.POST['name'], detail=request.POST['detail'], deadline=request.POST['deadline'], priolity=request.POST['priolity'])
-    return render(request, 'todos/end.html')
+    return render(request, 'todos/create_conf.html')
 
 # def EndView(request, todo_id):
 #     response = " end"
 #     return HttpResponse(response %todo_id)
 
-#Todoを削除する際の確認機能
+#Todoを削除する際の確認画面表示機能
 def deleteAlert(request, todo_id):
     todo = get_object_or_404(Todo, pk=todo_id)
     return render(request, 'todos/delete_alert.html',{'todo': todo})
